615ddb04e7ee3daa88eb54e2e3f473bb73197fa8
mpostaire <maxime_postaire@hotmail.fr>
2022-06-21 19:36:29 +0200

    zut

--- src/oof.c
+++ src/oof.c
@@ -1,4 +1,5 @@
 #include <gb/gb.h>
+#include <gb/cgb.h>
 
 #include <stdio.h>
 #include <gbdk/console.h>
@@ -7,7 +8,7 @@
 // Postion values are calculated as 16 bit numbers and their
 // lower 4 bits are dropped when applying them to the sprite
 
-uint8_t sprite_data[] = { 
+uint8_t sprite_data[] = {
     0x3C,0x3C,0x42,0x7E,0x99,0xFF,0xA9,0xFF,0x89,0xFF,0x89,0xFF,0x42,0x7E,0x3C,0x3C,
     0x3C,0x3C,0x42,0x7E,0xB9,0xFF,0x89,0xFF,0x91,0xFF,0xB9,0xFF,0x42,0x7E,0x3C,0x3C,
     0x3C,0x3C,0x42,0x7E,0x99,0xFF,0x89,0xFF,0x99,0xFF,0x89,0xFF,0x5A,0x7E,0x3C,0x3C,
@@ -22,17 +23,20 @@ uint8_t Jump;
 // main funxction
 void main(void) {
     // init palettes
-    BGP_REG = OBP0_REG = OBP1_REG = 0xE4;
+    BGP_REG = 0xE4;
+    OBP0_REG = 0xE4;
+    OBP1_REG = 0xE4;
 
     // load tile data into VRAM
     set_sprite_data(0, 4, sprite_data);
-    
+
     // set sprite tile
     set_sprite_tile(0, 0);
 
     // show bkg and sprites
-    SHOW_BKG; SHOW_SPRITES;
- 
+    SHOW_BKG;
+    SHOW_SPRITES;
+
     PosX = PosY = 64 << 4;
     Jump = SpdX = SpdY = 0;
 
@@ -66,19 +70,20 @@ void main(void) {
             Jump--;
         }
 
-        PosX += SpdX, PosY += SpdY; 
+        PosX += SpdX;
+        PosY += SpdY;
 
         // Translate to pixels and move sprite
         // Downshift by 4 bits to use the whole number values
         move_sprite(0, PosX >> 4, PosY >> 4);
 
-        // decelerate 
+        // decelerate
         if (SpdY >= 0) {
-            if (SpdY) SpdY--; 
-        } else SpdY ++;
+            if (SpdY) SpdY--;
+        } else SpdY++;
         if (SpdX >= 0) {
-            if (SpdX) SpdX--; 
-        } else SpdX ++;
+            if (SpdX) SpdX--;
+        } else SpdX++;
 
         // Done processing, yield CPU and wait for start of next frame (VBlank)
         wait_vbl_done();
