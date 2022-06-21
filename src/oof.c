db9218b1a43139418c5cea54eb0c65dd32cf21be
mpostaire <maxime_postaire@hotmail.fr>
2022-06-21 22:43:15 +0200

    added platform

--- src/oof.c
+++ src/oof.c
@@ -1,26 +1,29 @@
 #include <gb/gb.h>
 #include <gb/cgb.h>
 
-#include <stdio.h>
-#include <gbdk/console.h>
-
-// A simple sub-pixel / fixed point example
-// Postion values are calculated as 16 bit numbers and their
-// lower 4 bits are dropped when applying them to the sprite
+#include <stdint.h>
 
 uint8_t sprite_data[] = {
-    0x3C,0x3C,0x42,0x7E,0x99,0xFF,0xA9,0xFF,0x89,0xFF,0x89,0xFF,0x42,0x7E,0x3C,0x3C,
-    0x3C,0x3C,0x42,0x7E,0xB9,0xFF,0x89,0xFF,0x91,0xFF,0xB9,0xFF,0x42,0x7E,0x3C,0x3C,
-    0x3C,0x3C,0x42,0x7E,0x99,0xFF,0x89,0xFF,0x99,0xFF,0x89,0xFF,0x5A,0x7E,0x3C,0x3C,
-    0x3C,0x3C,0x42,0x7E,0xA9,0xFF,0xA9,0xFF,0xB9,0xFF,0x89,0xFF,0x42,0x7E,0x3C,0x3C
+    0x3C,0x3C,0x42,0x7E,0x99,0xFF,0xA9,0xFF,0x89,0xFF,0x89,0xFF,0x42,0x7E,0x3C,0x3C
+};
+
+// black tile
+uint8_t tile_data[] = {
+    0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,
+    0xFF,0xFF,0xFF,0x00,0xFF,0xFF,0xFF,0x00,0xFF,0xFF,0xFF,0x00,0xFF,0xFF,0xFF,0x00
+};
+
+uint8_t tile_map[] = {
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0
 };
 
 // sprite coords
-uint16_t PosX, PosY;
-int16_t SpdX, SpdY;
-uint8_t Jump;
+uint16_t pos_x, pos_y;
+int16_t vel_x, vel_y;
+uint8_t jump;
 
-// main funxction
+// main function
 void main(void) {
     // init palettes
     BGP_REG = 0xE4;
@@ -33,57 +36,61 @@ void main(void) {
     // set sprite tile
     set_sprite_tile(0, 0);
 
+    // Load Background tiles and then map
+    set_bkg_data(0, 2, tile_data);
+    set_bkg_tiles(0, 12, 20, 2, tile_map);
+
     // show bkg and sprites
     SHOW_BKG;
     SHOW_SPRITES;
 
-    PosX = PosY = 64 << 4;
-    Jump = SpdX = SpdY = 0;
+    pos_x = pos_y = 64 << 4;
+    jump = vel_x = vel_y = 0;
 
-    while(1) {
+    while (1) {
         // poll joypad
         uint8_t input = joypad();
 
         // game object
         if (input & J_UP) {
-            SpdY -= 2;
-            if (SpdY < -64) SpdY = -64;
+            vel_y -= 2;
+            if (vel_y < -64) vel_y = -64;
         } else if (input & J_DOWN) {
-            SpdY += 2;
-            if (SpdY > 64) SpdY = 64;
+            vel_y += 2;
+            if (vel_y > 64) vel_y = 64;
         }
         if (input & J_LEFT) {
-            SpdX -= 2;
-            if (SpdX < -64) SpdX = -64;
+            vel_x -= 2;
+            if (vel_x < -64) vel_x = -64;
         } else if (input & J_RIGHT) {
-            SpdX += 2;
-            if (SpdX > 64) SpdX = 64;
+            vel_x += 2;
+            if (vel_x > 64) vel_x = 64;
         }
-        if ((input & J_A) && (!Jump)) {
-            Jump = 3;
+        if ((input & J_A) && (!jump)) {
+            jump = 3;
         }
 
         // jump
-        if (Jump) {
-            SpdY -= 8;
-            if (SpdY < -32) SpdY = -32;
-            Jump--;
+        if (jump) {
+            vel_y -= 8;
+            if (vel_y < -32) vel_y = -32;
+            jump--;
         }
 
-        PosX += SpdX;
-        PosY += SpdY;
+        pos_x += vel_x;
+        pos_y += vel_y;
 
         // Translate to pixels and move sprite
         // Downshift by 4 bits to use the whole number values
-        move_sprite(0, PosX >> 4, PosY >> 4);
+        move_sprite(0, pos_x >> 4, pos_y >> 4);
 
         // decelerate
-        if (SpdY >= 0) {
-            if (SpdY) SpdY--;
-        } else SpdY++;
-        if (SpdX >= 0) {
-            if (SpdX) SpdX--;
-        } else SpdX++;
+        if (vel_y >= 0) {
+            if (vel_y) vel_y--;
+        } else vel_y++;
+        if (vel_x >= 0) {
+            if (vel_x) vel_x--;
+        } else vel_x++;
 
         // Done processing, yield CPU and wait for start of next frame (VBlank)
         wait_vbl_done();
